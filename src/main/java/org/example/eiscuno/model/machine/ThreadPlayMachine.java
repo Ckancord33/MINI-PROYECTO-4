2624dfcee84014521954a4561c03e767a2d9e29d
Ckancord33 <niccord33@gmail.com>
2024-12-09 19:42:23 -0500

    Ahora se pueden ver facilmente las cartas de la maquina (Ahora se ve la parte de atras de la carta para la maquina pero tambien se pueden ver las cartas de la maquina)

--- src/main/java/org/example/eiscuno/model/machine/ThreadPlayMachine.java
+++ src/main/java/org/example/eiscuno/model/machine/ThreadPlayMachine.java
@@ -1,33 +1,41 @@
 package org.example.eiscuno.model.machine;
 
+import javafx.application.Platform;
 import javafx.scene.image.ImageView;
+import org.example.eiscuno.controller.GameUnoController;
 import org.example.eiscuno.model.card.Card;
 import org.example.eiscuno.model.player.Player;
 import org.example.eiscuno.model.table.Table;
+import org.example.eiscuno.view.GameUnoStage;
+
+import java.io.IOException;
 
 public class ThreadPlayMachine extends Thread {
     private Table table;
     private Player machinePlayer;
     private ImageView tableImageView;
+    private volatile GameUnoController controller;
     private volatile boolean hasPlayerPlayed;
 
-    public ThreadPlayMachine(Table table, Player machinePlayer, ImageView tableImageView) {
+    public ThreadPlayMachine(Table table, Player machinePlayer, ImageView tableImageView, GameUnoController controller)throws IOException {
         this.table = table;
         this.machinePlayer = machinePlayer;
         this.tableImageView = tableImageView;
         this.hasPlayerPlayed = false;
+        this.controller = controller;
     }
 
     public void run() {
         while (true){
             if(hasPlayerPlayed){
                 try{
-                    Thread.sleep(2000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 // Aqui iria la logica de colocar la carta
                 putCardOnTheTable();
+                Platform.runLater(() -> controller.printCardsMachinePlayer());
                 hasPlayerPlayed = false;
             }
         }
@@ -36,11 +44,21 @@ public class ThreadPlayMachine extends Thread {
     private void putCardOnTheTable(){
         int index = (int) (Math.random() * machinePlayer.getCardsPlayer().size());
         Card card = machinePlayer.getCard(index);
+        machinePlayer.removeCard(findPosCardsMachinePlayer(card));
         table.addCardOnTheTable(card);
         tableImageView.setImage(card.getImage());
     }
 
     public void setHasPlayerPlayed(boolean hasPlayerPlayed) {
         this.hasPlayerPlayed = hasPlayerPlayed;
+    }
+
+    public Integer findPosCardsMachinePlayer(Card card) {
+        for (int i = 0; i < this.machinePlayer.getCardsPlayer().size(); i++) {
+            if (this.machinePlayer.getCardsPlayer().get(i).equals(card)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
